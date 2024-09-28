f8ea5a38dd4899f9f10ee2a25f9b71c3c0344e4a
KrivosheyYuriy <lonespy@yandex.ru>
2024-09-28 13:18:21 +0300

    REPL

--- app/main.c
+++ app/main.c
@@ -3,14 +3,16 @@
 
 int main() {
     // Uncomment this block to pass the first stage
-    printf("$ ");
-    fflush(stdout);
 
     // Wait for user input
     char input[100];
-    fgets(input, 100, stdin);
+    while (2) {
+        printf("$ ");
+        fflush(stdout);
+        fgets(input, 100, stdin);
 
-    input[strlen(input) - 1] = '\0';
-    printf("%s: command not found\n", input);
+        input[strlen(input) - 1] = '\0';
+        printf("%s: command not found\n", input);
+    }
     return 0;
 }
